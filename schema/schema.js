81d18476f43e7f329c6dceb9c7d02390375e1c3c
HassanKhan123 <hk2602809@gmail.com>
2021-05-22 07:30:22 +0500

    update mutation added

--- schema/schema.js
+++ schema/schema.js
@@ -97,6 +97,20 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((res) => res.data);
+      },
+    },
     deleteUser: {
       type: UserType,
       args: {
